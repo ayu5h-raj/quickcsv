e5e7804f24a8ed7724b34e834717dca6ed16e988
ayu5h-raj <ping2r3j@gmail.com>
2026-01-04 19:05:53 +0000

    Deploying to gh-pages from @ ayu5h-raj/quickcsv@db19917951a3f4253c583c61bc62d1b6abe8570d 🚀

--- quickcsv-f5090e49429c10ff.js
+++ quickcsv-f5090e49429c10ff.js
@@ -307,12 +307,12 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
-function __wasm_bindgen_func_elem_1057(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_1057(arg0, arg1, addHeapObject(arg2));
-}
-
 function __wasm_bindgen_func_elem_631(arg0, arg1) {
     wasm.__wasm_bindgen_func_elem_631(arg0, arg1);
+}
+
+function __wasm_bindgen_func_elem_1057(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_1057(arg0, arg1, addHeapObject(arg2));
 }
 
 function __wasm_bindgen_func_elem_731(arg0, arg1, arg2) {
